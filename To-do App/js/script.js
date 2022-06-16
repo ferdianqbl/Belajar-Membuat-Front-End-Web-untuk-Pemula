7eebbf50f1027ecbfc86a12c2d389d218848e8f2
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-06-16 13:46:00 +0700

    edit todo

--- To-do App/js/script.js
+++ To-do App/js/script.js
@@ -1,11 +1,17 @@
 const todos = [];
 const RENDER_EVENT = 'render-todo';
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
+
 document.addEventListener('DOMContentLoaded', function () {
   const submitForm = document.getElementById('form');
   submitForm.addEventListener('submit', function (event) {
     event.preventDefault();
     addTodo();
   });
+  if (isStorageExist()) {
+    loadDataFromStorage();
+  }
 });
 
 document.addEventListener(RENDER_EVENT, function () {
@@ -20,6 +26,23 @@ document.addEventListener(RENDER_EVENT, function () {
     else completedTODOList.append(todoElement);
   }
 });
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+
+  if (data !== null) {
+    for (const todo of data) {
+      todos.push(todo);
+    }
+  }
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
 
 findTodo = todoId => {
   for (const todo of todos) {
@@ -62,6 +85,7 @@ function addTodo() {
   todos.push(todoObject);
 
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
 
 function makeTodo(todoObject) {
@@ -117,6 +141,7 @@ function addTaskToCompleted(todoId) {
 
   todoTarget.isCompleted = true;
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
 
 function removeTaskFromCompleted(todoId) {
@@ -126,6 +151,7 @@ function removeTaskFromCompleted(todoId) {
 
   todos.splice(todoTarget, 1);
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
 
 
@@ -136,4 +162,20 @@ function undoTaskFromCompleted(todoId) {
 
   todoTarget.isCompleted = false;
   document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function saveData() {
+  if (isStorageExist()) {
+    const parsed = JSON.stringify(todos);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+  }
+}
+
+function isStorageExist() {
+  if (typeof localStorage !== 'undefined')
+    return true;
+  alert('Your browser does not support local storage');
+  return false;
 }
